b459cc5621171e340a1a12e365dc33f30aea126f
RaeesKhan14 <10640640@bradfordcollege.ac.uk>
2025-09-16 14:06:47 +0100

    Create Anything2.py

---
+++ Anything2.py
@@ -0,0 +1,9 @@
+numbers = [12, 5, 8, 22, 7, 19, 30, 4, 10, 16,]
+
+print("Largest:", max(numbers))
+print("Smallest:", min(numbers))
+print(Average:", sum(numbers) / len(numbers))
+
+num_set= set(numbers)
+print(Orginal list:", numbers)
+print("Converted set:", num_set)
